59b7504260738b46274bbf6923edc5483240b3e9
David Masad <david.masad@gmail.com>
2015-12-02 15:44:51 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pyObjectClipboard",
+    version="0.1",
+    description="Copy and paste Python objects",
+    author="David Masad",
+    author_email="david.masad@gmail.com",
+    install_requires=['pyperclip'],
+    py_modules=['pyobjclipboard']
+    )
